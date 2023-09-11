3e8d139e11a5285a5ca347d09c0fb17c6c09eaa5
cyPark95 <parkcy1007@gmail.com>
2023-09-11 15:45:06 +0900

    재귀함수가 뭔가요?

---
+++ chanyeong/0911/0911_1.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static StringBuilder sb;
+
+    public static void main(String[] args) {
+        input();
+        solution();
+    }
+
+    private static void input() {
+        ScannerReader sr = new ScannerReader();
+        N = sr.nextInt();
+        sb = new StringBuilder();
+    }
+
+    private static void solution() {
+        sb.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.").append("\n");
+        recursive(0);
+
+        System.out.println(sb);
+    }
+
+    private static void recursive(int i) {
+        String prefix = "_".repeat(i * 4);
+        if (i == N) {
+            sb.append(prefix).append("\"재귀함수가 뭔가요?\"").append("\n");
+            sb.append(prefix).append("\"재귀함수는 자기 자신을 호출하는 함수라네\"").append("\n");
+        } else {
+            sb.append(prefix).append("\"재귀함수가 뭔가요?\"").append("\n");
+            sb.append(prefix).append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.").append("\n");
+            sb.append(prefix).append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.").append("\n");
+            sb.append(prefix).append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"").append("\n");
+            recursive(i + 1);
+        }
+        sb.append(prefix).append("라고 답변하였지.").append("\n");
+    }
+
+    static class ScannerReader {
+
+        private final BufferedReader br;
+        private StringTokenizer st;
+
+        public ScannerReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public String readLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "";
+        }
+
+        public String next() {
+            while (st == null || !st.hasMoreElements()) {
+                st = new StringTokenizer(readLine());
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
