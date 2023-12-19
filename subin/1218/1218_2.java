4cc1f2ca9323a3daba580e77df70d91f4a742a2e
Su-daa <97935558+Su-daa@users.noreply.github.com>
2023-12-19 14:22:57 +0900

    삼총사

---
+++ subin/1218/1218_2.java
@@ -0,0 +1,18 @@
+package fc1218;
+
+//삼총사
+public class pg131705 {
+    public static void main(String[] args) {
+        int[] number = {-2, 3, 0, 2, -5};
+        int answer =0;
+        for (int i = 0; i < number.length; i++) {
+            for (int j = i + 1; j < number.length; j++) {
+                for (int k = j + 1; k < number.length; k++) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        answer++;
+                    }
+                }
+            }
+        }
+    }
+}
