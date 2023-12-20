18b4591ca15a59de5b6588787d2cdf47aaa829b9
유현 <yh7119@gmail.com>
2023-12-20 11:46:21 +0900

    없는 숫자 더하기

--- hyun/1220/1220_2.java
+++ hyun/1220/1220_2.java
@@ -2,15 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] numbers) {
-        Set<Integer> set = new HashSet<>();
-        int answer = 0;
+        int answer = 45;
+        boolean[] v = new boolean[10];
+
         for (int i : numbers) {
-            set.add(i);
+            if (!v[i]) answer -= i;
+            else v[i] = true;
         }
-        for (int n = 0; n <= 9; n++) {
-            if (set.contains(n)) continue;
-            answer += n;
-        }
+
         return answer;
     }
 }
