ff640b6a712c36eea1e9f7c732650d9216618585
Su-daa <97935558+Su-daa@users.noreply.github.com>
2023-12-27 14:00:37 +0900

    체육복

---
+++ subin/1222/1222_2.java
@@ -0,0 +1,39 @@
+package fc1222;
+
+import java.util.Arrays;
+
+//체육복
+public class pg42862 {
+    public static void main(String[] args) {
+        int n =5;
+        int[] lost = {2, 4};
+        int[] reserve = {1, 3, 5};
+        int answer = 0;
+
+        Arrays.sort(reserve);
+        Arrays.sort(lost);
+
+        answer = n - lost.length;
+
+        for (int i = 0; i < lost.length; i++) {
+			for (int j = 0; j < reserve.length; j++) {
+				if (lost[i] == reserve[j]) {
+					answer++;
+					lost[i] = -1;
+					reserve[j] = -1;
+                    break;
+				}
+			}
+		}
+
+        for (int i = 0; i < lost.length; i++) {
+			for (int j = 0; j < reserve.length; j++) {
+				if (lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]) {
+					answer++;
+					reserve[j] = -1;
+					break;
+				}
+			}
+		}
+    }
+}
